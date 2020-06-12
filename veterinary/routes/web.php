452783efc2d33dff1b59eba69aee273efbb24100
Kristína Janeková <yanekkris@gmail.com>
2020-06-12 12:14:33 +0100

    Controllers/Routes

--- veterinary/routes/web.php
+++ veterinary/routes/web.php
@@ -20,4 +20,24 @@ Route::get('/', function () {
 
 //Owner routes
 
+Route::get('/owners', 'OwnerController@index')->name('owners.index');
+Route::get('/owners', 'OwnerController@search')->name('owners.search');
+
+Route::get('/owners/{owner_id}', 'OwnerController@show')->where('owner_id', '[0-9]+');
+
+Route::get('/owners/create',         'OwnerController@create')->name('owners.create');
+Route::post('/owners',               'OwnerController@store')->name('owners.store');
+Route::get('/owners/{owner_id}/edit', 'OwnerController@edit')->name('owners.edit');
+Route::post('/owners/{owner_id}',     'OwnerController@update')->name('owners.update');
+
 //Animal routes
+
+Route::get('/animal', 'OwnerController@index')->name('animal.index');
+Route::get('/animal', 'OwnerController@search')->name('animal.search');
+
+Route::get('/animal/{owner_id}', 'OwnerController@show')->where('owner_id', '[0-9]+');
+
+Route::get('/animal/create',         'OwnerController@create')->name('animal.create');
+Route::post('/animal',               'OwnerController@store')->name('animal.store');
+Route::get('/animal/{owner_id}/edit', 'OwnerController@edit')->name('animal.edit');
+Route::post('/animal/{owner_id}',     'OwnerController@update')->name('animal.update');
